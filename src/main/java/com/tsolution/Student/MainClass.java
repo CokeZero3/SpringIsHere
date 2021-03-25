735b4798b478b4b2fcb22de0ffd595f08aca9589
wonho.jeong <jeongwh0310@gmail.com>
2021-03-25 22:07:59 +0900

    spring

--- src/main/java/com/tsolution/Student/MainClass.java
+++ src/main/java/com/tsolution/Student/MainClass.java
@@ -1,7 +1,17 @@
 package com.tsolution.Student;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(); //생성
+		ctx.load("classpath:applicationCTX4_Student2.xml"); //설정
+		ctx.refresh();
 		
+		Student student = ctx.getBean("student", Student.class); //사용
+		System.out.println("이름: " + student.getName());
+		System.out.println("나이: " + student.getAge());
+		
+		ctx.close();
 	}
 }
